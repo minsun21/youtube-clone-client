1d1eb4fb59f6b5f8ac72a69ccff1bf9f0e8a7d77
minsun21 <mspark0429@daum.net>
2020-10-25 16:32:17 +0900

    Header css

---
+++ README.md
@@ -0,0 +1,25 @@
+## Install
+
+1. react-router- dom
+npm install react-router-dom
+
+
+## Setting
+
+1. FontAwesome cdn in to index.html
+
+2. Google Font
+index.html, index.css
+
+3. global css
+
+
+## Coding
+
+1. Header
+
+1) icons -> ok
+2) img -> ok
+3) button 
+3) dropdown
+3) sidebar
--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-
+      {/* Header */}
+      <div className="header">
+        <div className="header-left">
+          <i className="fas fa-bars"></i>
+          <span className="header-logo">
+            <i className="fab fa-youtube"></i>
+            <span>YouTube</span>
+          </span>
+        </div>
+        <div className="header-right">
+          <i className="fas fa-search"></i>
+          <i className="fas fa-th"></i>
+          <i className="fas fa-video"></i>
+          <i className="fas fa-bell"></i>
+        </div>
+      </div>
     </div>
   );
 }
